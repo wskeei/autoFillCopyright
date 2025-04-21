89cdb182e61555e80f92d5795c2a30fb8ce21ee9
zeiy <1814897272@qq.com>
2025-04-21 12:52:37 +0800

    运行的硬件软件

--- popup.js
+++ popup.js
@@ -4,13 +4,24 @@ document.getElementById('autofill-btn').addEventListener('click', async () => {
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
     func: () => {
-      // 填写内容
-      const autofillContent = "四核intel i5 CPU，16GB内存，512固态硬盘，10Mbps网络带宽";
-      const textarea = document.querySelector('.fillin_info .hd-text-area.large textarea.large');
-      if (textarea) {
-        textarea.value = autofillContent;
-        textarea.dispatchEvent(new Event('input', { bubbles: true }));
-        textarea.dispatchEvent(new Event('change', { bubbles: true }));
+      // 第一个表单内容
+      const autofillContent1 = "四核intel i5 CPU，16GB内存，512固态硬盘，10Mbps网络带宽";
+      // 第二个表单内容
+      const autofillContent2 = "服务端内存2G以上，硬盘空间不低于40G； 客户端4G及以上内存，硬盘空间16G及以上";
+      // 获取所有匹配的 textarea
+      const textareas = document.querySelectorAll('.fillin_info .hd-text-area.large textarea.large');
+      if (textareas.length >= 2) {
+        textareas[0].value = autofillContent1;
+        textareas[0].dispatchEvent(new Event('input', { bubbles: true }));
+        textareas[0].dispatchEvent(new Event('change', { bubbles: true }));
+        textareas[1].value = autofillContent2;
+        textareas[1].dispatchEvent(new Event('input', { bubbles: true }));
+        textareas[1].dispatchEvent(new Event('change', { bubbles: true }));
+      } else if (textareas.length === 1) {
+        textareas[0].value = autofillContent1;
+        textareas[0].dispatchEvent(new Event('input', { bubbles: true }));
+        textareas[0].dispatchEvent(new Event('change', { bubbles: true }));
+        alert('只找到一个表单，第二个未填写');
       } else {
         alert('未找到目标 textarea 元素');
       }
