4ed55c165df4f85cff3bbf7befee952f0b98e87e
zeiy <1814897272@qq.com>
2025-04-21 13:04:16 +0800

    开发软件的操作系统、软件开发环境

--- popup.js
+++ popup.js
@@ -8,20 +8,49 @@ document.getElementById('autofill-btn').addEventListener('click', async () => {
       const autofillContent1 = "四核intel i5 CPU，16GB内存，512固态硬盘，10Mbps网络带宽";
       // 第二个表单内容
       const autofillContent2 = "服务端内存2G以上，硬盘空间不低于40G； 客户端4G及以上内存，硬盘空间16G及以上";
+      // 第三个表单内容
+      const autofillContent3 = "Windows 10操作系统";
+      // 第四个表单内容
+      const autofillContent4 = "开发环境：node.js16.17.1，Vue3 开发工具：VsCode";
       // 获取所有匹配的 textarea
       const textareas = document.querySelectorAll('.fillin_info .hd-text-area.large textarea.large');
-      if (textareas.length >= 2) {
+      if (textareas.length >= 4) {
         textareas[0].value = autofillContent1;
         textareas[0].dispatchEvent(new Event('input', { bubbles: true }));
         textareas[0].dispatchEvent(new Event('change', { bubbles: true }));
         textareas[1].value = autofillContent2;
         textareas[1].dispatchEvent(new Event('input', { bubbles: true }));
         textareas[1].dispatchEvent(new Event('change', { bubbles: true }));
+        textareas[2].value = autofillContent3;
+        textareas[2].dispatchEvent(new Event('input', { bubbles: true }));
+        textareas[2].dispatchEvent(new Event('change', { bubbles: true }));
+        textareas[3].value = autofillContent4;
+        textareas[3].dispatchEvent(new Event('input', { bubbles: true }));
+        textareas[3].dispatchEvent(new Event('change', { bubbles: true }));
+      } else if (textareas.length === 3) {
+        textareas[0].value = autofillContent1;
+        textareas[0].dispatchEvent(new Event('input', { bubbles: true }));
+        textareas[0].dispatchEvent(new Event('change', { bubbles: true }));
+        textareas[1].value = autofillContent2;
+        textareas[1].dispatchEvent(new Event('input', { bubbles: true }));
+        textareas[1].dispatchEvent(new Event('change', { bubbles: true }));
+        textareas[2].value = autofillContent3;
+        textareas[2].dispatchEvent(new Event('input', { bubbles: true }));
+        textareas[2].dispatchEvent(new Event('change', { bubbles: true }));
+        alert('只找到三个表单，第四个未填写');
+      } else if (textareas.length === 2) {
+        textareas[0].value = autofillContent1;
+        textareas[0].dispatchEvent(new Event('input', { bubbles: true }));
+        textareas[0].dispatchEvent(new Event('change', { bubbles: true }));
+        textareas[1].value = autofillContent2;
+        textareas[1].dispatchEvent(new Event('input', { bubbles: true }));
+        textareas[1].dispatchEvent(new Event('change', { bubbles: true }));
+        alert('只找到两个表单，第三、第四个未填写');
       } else if (textareas.length === 1) {
         textareas[0].value = autofillContent1;
         textareas[0].dispatchEvent(new Event('input', { bubbles: true }));
         textareas[0].dispatchEvent(new Event('change', { bubbles: true }));
-        alert('只找到一个表单，第二个未填写');
+        alert('只找到一个表单，第二、第三、第四个未填写');
       } else {
         alert('未找到目标 textarea 元素');
       }
